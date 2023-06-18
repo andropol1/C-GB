3b37f64b752a5fc7d878e4b8a504a170a95141cd
andropol1 <andrew.kondratenkov@gmail.com>
2023-06-18 20:02:19 +0300

    Final homework

---
+++ task66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int SumOfNaturals(int m, int n, ref int sum)
+{
+   
+   for (int i = m; i <= n; i++)
+   {
+   sum += i;
+   }
+   return sum;
+   
+}
+SumOfNaturals(m, n, ref sum);
+Console.WriteLine(sum);
---
+++ task68/Program.cs
@@ -0,0 +1,25 @@
+﻿int accerman(int m, int n)
+{
+   if (m ==0)
+   {
+      return n + 1;
+   }
+   else if (m > 0 && n == 0)
+   {
+      return accerman(m-1, 1);
+   }
+   else if (m > 0 && n > 0)
+   {
+      return accerman(m-1, accerman(m, n-1));
+   }
+   else
+   {
+      return -1;
+   }
+}
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(accerman(m,n));
+
